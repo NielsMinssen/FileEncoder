412453994e0b596796fa7097e202665be2a05f73
Niels <niels.minssen@etu.uca.fr>
2023-11-06 13:20:38 +0800

    detection d'encoding valide

--- FileEncoderProject/FileEncoder/views.py
+++ FileEncoderProject/FileEncoder/views.py
@@ -11,27 +11,43 @@ def file_upload_view(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            # Process the uploaded file
             uploaded_file = request.FILES['file']
-            file_content = uploaded_file.read()
+            action = request.POST.get('action')
 
-            # Use chardet to detect the encoding
-            detected_encoding = chardet.detect(file_content)['encoding']
+            if action == 'detect':
+                # Read the file content
+                content = uploaded_file.read()
+                # Detect encoding
+                result = chardet.detect(content)
+                encoding = result['encoding']
+                confidence = result['confidence']
+                # Redirect to a new URL or render the same page with the detected encoding
+                # You can pass the detected encoding in the context or as a GET parameter
+                return render(request, 'FileEncoder/upload.html', {
+                    'form': form,
+                    'detected_encoding': encoding,
+                    'confidence': confidence
+                })
+            elif action == 'encode':
+                file_content = uploaded_file.read()
 
-            # Assume a default encoding if chardet is not sure
-            original_encoding = detected_encoding if detected_encoding is not None else 'utf-8'
+                # Use chardet to detect the encoding
+                detected_encoding = chardet.detect(file_content)['encoding']
 
-            # You might want to reset the file read pointer to the start if you'll read it again
-            uploaded_file.seek(0)
+                # Assume a default encoding if chardet is not sure
+                original_encoding = detected_encoding if detected_encoding is not None else 'utf-8'
 
-            # Convert the encoding
-            new_file_path = convert_file_encoding(uploaded_file, original_encoding, request.POST['encoding'])
+                # You might want to reset the file read pointer to the start if you'll read it again
+                uploaded_file.seek(0)
 
-            # Prepare response to download the new file
-            with open(new_file_path, 'rb') as f:
-                response = HttpResponse(f, content_type='application/octet-stream')
-                response['Content-Disposition'] = f'attachment; filename="{uploaded_file.name}"'
-                return response
+                # Convert the encoding
+                new_file_path = convert_file_encoding(uploaded_file, original_encoding, request.POST['encoding'])
+
+                # Prepare response to download the new file
+                with open(new_file_path, 'rb') as f:
+                    response = HttpResponse(f, content_type='application/octet-stream')
+                    response['Content-Disposition'] = f'attachment; filename="{uploaded_file.name}"'
+                    return response
     else:
         form = FileUploadForm()
 
